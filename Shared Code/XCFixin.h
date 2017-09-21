588c9521377c630cf5b9243bdf658eb17d77b07d
Erik Olofsson <erik@olofsson.info>
2017-09-21 11:33:37 +0200

    Fix version expansion

--- Shared Code/XCFixin.h
+++ Shared Code/XCFixin.h
@@ -65,7 +65,8 @@
     }                                                   \
 })
 
-#define XCFixinXcodeVersionedHelper(_Name, _Version) _Name##_Version
+#define XCFixinXcodeVersionedHelper2(_Name, _Version) _Name##_Version
+#define XCFixinXcodeVersionedHelper(_Name, _Version) XCFixinXcodeVersionedHelper2(_Name, _Version)
 #define XCFixinXcodeVersioned(_Name) XCFixinXcodeVersionedHelper(_Name, XCODE_VERSION_MINOR)
 
 BOOL XCFixinShouldLoad(BOOL _LoadInXcodeBuild);
